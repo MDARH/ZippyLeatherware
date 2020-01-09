f8d512bac2a3acf3d0ece3741cc68437f8e109d3
Abdur Razzaque <mdarh411@gmail.com>
2020-01-09 18:39:57 +0600

    product create section fix default selection

--- packages/Webkul/Admin/src/Resources/views/catalog/products/create.blade.php
+++ packages/Webkul/Admin/src/Resources/views/catalog/products/create.blade.php
@@ -70,7 +70,7 @@
                             <select class="control" v-validate="'required'" id="attribute_family_id" name="attribute_family_id" {{ $familyId ? 'disabled' : '' }} data-vv-as="&quot;{{ __('admin::app.catalog.products.familiy') }}&quot;">
                                 <option value=""></option>
                                 @foreach ($families as $family)
-                                    <option value="{{ $family->id }}" {{ ($familyId == $family->id || old('attribute_family_id') == $family->id) ? 'selected' : '' }}>{{ $family->name }}</option>
+                                    <option value="{{ $family->id }}" {{ ($familyId == $family->id || old('ute_family_iattribd') == $family->id) ? '' : 'selected' }}>{{ $family->name }}</option>
                                     @endforeach
                             </select>
 
